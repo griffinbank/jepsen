5bf6d9083dada9c27dfb5d0d9826e4a096aa3b39
Dan Meyers <d.meyers@infinitefunspace.co.uk>
2022-01-28 21:49:47 +0000

    Add Banksy additions
    Making sure we have Clojure on the node containers, setting resource
    limits, adjusting the init script so it actually works as intended when
    the number of nodes is changed, etc.

--- docker/control/init.sh
+++ docker/control/init.sh
@@ -2,21 +2,35 @@
 
 : "${SSH_PRIVATE_KEY?SSH_PRIVATE_KEY is empty, please use up.sh}"
 : "${SSH_PUBLIC_KEY?SSH_PUBLIC_KEY is empty, please use up.sh}"
+: "${NODE_COUNT?NODE_COUNT is empty, please use up.sh}"
 
-if [ ! -f ~/.ssh/known_hosts ]; then
-    mkdir -m 700 ~/.ssh
-    echo $SSH_PRIVATE_KEY | perl -p -e 's/↩/\n/g' > ~/.ssh/id_rsa
-    chmod 600 ~/.ssh/id_rsa
-    echo $SSH_PUBLIC_KEY > ~/.ssh/id_rsa.pub
-    echo > ~/.ssh/known_hosts
-    # Get nodes list
-    sort -V /var/jepsen/shared/nodes > ~/nodes
-    # Scan SSH keys
-    while read node; do
-      ssh-keyscan -t rsa $node >> ~/.ssh/known_hosts
-      ssh-keyscan -t ed25519 $node >> ~/.ssh/known_hosts
-    done <~/nodes
+if [ ! -d ~/.ssh ]; then
+  mkdir -m 700 ~/.ssh
 fi
+
+if [ ! -f ~/.ssh/id_rsa ]; then
+  echo "${SSH_PRIVATE_KEY}" | perl -p -e 's/↩/\n/g' > ~/.ssh/id_rsa
+  chmod 600 ~/.ssh/id_rsa
+fi
+
+if [ ! -f ~/.ssh/id_rsa.pub ]; then
+  echo "${SSH_PUBLIC_KEY}" | perl -p -e 's/↩/\n/g' > ~/.ssh/id_rsa.pub
+fi
+
+# Remake known_hosts every time, to allow for changing node counts
+echo > ~/.ssh/known_hosts
+# Wait for all the hosts to report their hostnames.
+# Using a string comparison to handle file not being present yet
+while [ "$(wc -l /var/jepsen/shared/nodes | awk '{print $1;}')" != "${NODE_COUNT}" ]; do sleep 1; done
+# Get nodes list
+sort -V /var/jepsen/shared/nodes > ~/nodes
+# Scan SSH keys
+while read -r node; do
+  # Wait for the host to come up correctly
+  while ssh-keyscan -t rsa "${node}" 2>&1 | grep "Connection refused"; do sleep 1; done
+  ssh-keyscan -t rsa "${node}" 2>/dev/null >> ~/.ssh/known_hosts
+  ssh-keyscan -t ed25519 "${node}" 2>/dev/null >> ~/.ssh/known_hosts
+done < ~/nodes
 
 # TODO: assert that SSH_PRIVATE_KEY==~/.ssh/id_rsa
 
